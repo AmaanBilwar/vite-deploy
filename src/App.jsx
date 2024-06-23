031acc944b548e3ddf909192d2a7787127c048d1
Amaan <121273095+AmaanBilwar@users.noreply.github.com>
2024-06-23 00:43:35 -0400

    add: basic frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import logo from './assets/headshot-image.png'
+import { Link } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center align-center text-4xl font-semibold p-2'>
+        <h1>Web Application Programming and Hacking</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='flex justify-center align-center text-4xl font-normal p-2'>
+        <h2>Front-end Web Development Lab</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='flex justify-center align-center text-4xl font-light p-2'>
+        <h3>Instructor: Dr. Phu Phung</h3>
+      </div>
+
+     <div className='grid grid-cols-2 auto-rows-[300px] my-4'>
+      <div className='flex justify-center w-3/4  bg-stone-300'>
+        Student: Amaan Bilwar
+      <img src={logo} alt="headshot"  className='w-40 h-40'/>
+      </div>
+      
+      <div className='flex justify-center bg-zinc-300'>
+      A simple HTML page with
+      <div className='ml-2 '>
+        <ul>
+          <li>
+            <a href="https://react.dev/" target='_blank' className='rounded-lg bg-grey-50 text-decoration-line: no-underline hover:underline decoration-blue-400'>REACT framework</a>
+          </li>
+          <li>
+            <a href="https://www.javascript.com/" target='_blank' className='rounded-lg bg-grey-50 text-decoration-line: no-underline hover:underline decoration-orange-400'>Javascript</a>
+          </li>
+          <li>
+            <a href="https://jquery.com/" target='_blank' className='rounded-lg bg-grey-50 text-decoration-line: no-underline hover:underline decoration-slate-400'>JQuery</a>
+          </li>
+          <li>
+            <a href="https://tailwindcss.com/" target='_blank' className='rounded-lg bg-grey-50 text-decoration-line: no-underline hover:underline decoration-blue-600'>Tailwindcss :))</a>
+          </li>
+        </ul>
+        </div>
+      </div>
+     </div>
+      <hr />
+      
     </>
   )
 }
