b68a09d3b5263252e038ac7fc0da07d670efb1ac
Amaan <121273095+AmaanBilwar@users.noreply.github.com>
2024-06-24 13:33:58 -0400

    fixed: minor changes to Homepage

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,11 +4,8 @@ const Resume = () => {
   return (
     <>
       <div>
-        <h1
-          className="flex justify-center text-slate-400 p-12
-         text-xl"
-        >
-          If you were no directed to my resume, Click on the "Resume" again :)
+        <h1 className="flex justify-center p-12 text-xl text-slate-400">
+          If you were not directed to my resume, Click on the "Resume" again :)
         </h1>
       </div>
     </>
