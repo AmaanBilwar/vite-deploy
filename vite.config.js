8647deeb0a26a1059568b9525eb409aec79725cc
Amaan <121273095+AmaanBilwar@users.noreply.github.com>
2024-07-10 21:30:08 -0400

    added vite-plugin-radar for page tracking

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vite-deploy/'
-})
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: "G-QP9T8W99WN",
+      },
+    }),
+  ],
+  base: "/vite-deploy/",
+});
